98fa3fb0dced7380b77e2462adc3a12260c7bc6d
joelmurray0 <144688121+joelmurray0@users.noreply.github.com>
2023-12-19 17:57:34 +0000

    game logic

---
+++ mastermind.py
@@ -0,0 +1,34 @@
+import numpy
+import random
+
+class Mastermind:
+     CORRECT = 0
+     IN = 1
+     WRONG = 2
+
+     def __init__(self, slots, options, guesses) -> None:
+          self._slots = slots
+          self._options = options
+          self._guesses = guesses
+          self._answer = self._generate_answer()
+     
+     def _generate_answer(self):
+          return tuple([random.randint(1,self._options) for i in range(self._slots)])
+
+     def check_guess(self, guess):
+          comparison = numpy.subtract(guess, self._answer)
+          output = [self.CORRECT for i in range(self._slots)]
+
+          for i in range(self._slots):
+               print(i)
+               if comparison[i] != self.CORRECT:
+                    if guess[i] in self._answer:
+                         output[i] = self.IN
+                    else:
+                         output[i] = self.WRONG
+          return tuple(output)
+     
+m = Mastermind(4,4,4)
+
+print(m._answer)
+print(m.check_guess((1,2,3,4)))
